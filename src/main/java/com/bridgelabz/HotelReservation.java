f75ffacd1666538930cc4fe3f5e4319eacb58a7e
amit <amitpanika97@gmail.com>
2022-07-13 00:14:41 +0530

    Test For File Operation

--- src/main/java/com/bridgelabz/HotelReservation.java
+++ src/main/java/com/bridgelabz/HotelReservation.java
@@ -1,2 +1,38 @@
-package com.bridgelabz;public class HotelReservation {
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HotelReservation {
+    String hotelName;
+    int rating;
+    double regularCustomerRate;
+    Scanner sc = new Scanner(System.in);
+    /**
+     * Creating ArrayList of Hotel named hotelList of Hotel type
+     */
+    ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+    /**
+     * hotel - Instance (object) of Hotel Class
+     */
+    Hotel hotel;
+    public boolean addHotel() {
+
+        System.out.println("Enter hotel name:");
+        hotelName = sc.next();
+        System.out.println("Enter hotel rating:");
+        rating = sc.nextInt();
+        System.out.println("Enter regular customer rate:");
+        regularCustomerRate = sc.nextDouble();
+        hotel = new Hotel(hotelName, rating, regularCustomerRate);
+        return hotelList.add(hotel);
+    }
+
+    /**
+     * Method to Print ArrayList hotelList
+     */
+    public void printHotelList() {
+        System.out.println(hotelList);
+    }
+
 }
--- src/main/java/com/bridgelabz/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/HotelReservationMain.java
@@ -1,2 +1,10 @@
-package com.bridgelabz;public class HotelreservationMain {
+package com.bridgelabz;
+
+public class HotelReservationMain {
+    public static void main(String[] args) {
+        System.out.println(" Welcome To Hotel Reservation System");
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.addHotel();
+        hotelReservation.printHotelList();
+    }
 }
